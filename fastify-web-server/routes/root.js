83c2e8bbadbc32465a1372efe5434e1692078a87
DavideDR <d.dr@libero.it>
2022-11-21 11:33:19 +0100

    Adds hello route

--- fastify-web-server/routes/root.js
+++ fastify-web-server/routes/root.js
@@ -1,7 +1,21 @@
 'use strict'
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
+const root = `<html>
+<head>
+  <style>
+   body { background: #333; margin: 1.25rem }
+   a { color: yellow; font-size: 2rem; font-family: sans-serif }
+  </style>
+</head>
+<body>
+  <a href='/hello'>Hello</a>
+</body>
+</html>
+`
+module.exports = async function(fastify, opts) {
+    fastify.get('/', async function(request, reply) {
+        //return { root: true } // when an object is returned, F. converts it to a JSON payloads that is sent as a response
+        reply.type('text/html')
+        return root
+    })
 }
