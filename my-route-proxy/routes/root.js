4c86688f710413ab7ccdaa717c792bb93ab84ffc
DavideDR <d.dr@libero.it>
2023-02-21 17:54:28 +0100

    Completes Single-Route, Multi-Origin Proxy (2)

--- my-route-proxy/routes/root.js
+++ my-route-proxy/routes/root.js
@@ -1,4 +1,11 @@
 'use strict'
+const { Readable } = require('stream')
+
+async function* upper(res) {
+    for await (const chunk of res) {
+        yield chunk.toString().toUpperCase()
+    }
+}
 
 module.exports = async function(fastify, opts) {
     fastify.get('/', async function(request, reply) {
@@ -8,6 +15,10 @@ module.exports = async function(fastify, opts) {
         } catch (err) {
             throw fastify.httpErrors.badRequest()
         }
-        return reply.from(url)
+        return reply.from(url, {
+            onResponse(request, reply, res) {
+                reply.send(Readable.from(upper(res)))
+            }
+        })
     })
 }
