4658b62e71852d98d37a35e8c5b028918b6a8677
DavideDR <d.dr@libero.it>
2023-02-21 17:05:45 +0100

    Completes Consuming and Aggregating Services

--- Consuming and Aggregating Services/consuming-service/routes/root.js
+++ Consuming and Aggregating Services/consuming-service/routes/root.js
@@ -15,11 +15,17 @@ module.exports = async function(fastify, opts) {
             const bicycle = await got(`${bicycleSrv}/${id}`).json()
             const brand = await got(`${brandSrv}/${id}`).json()
             */
-        const [bicycle, brand] = await Promise.all([got(`${bicycleSrv}/${id}`).json(), await got(`${brandSrv}/${id}`).json()])
-        return {
-            id: bicycle.Id,
-            color: bicycle.color,
-            brand: brand.name
+        try {
+            const [bicycle, brand] = await Promise.all([got(`${bicycleSrv}/${id}`).json(), await got(`${brandSrv}/${id}`).json()])
+            return {
+                id: bicycle.Id,
+                color: bicycle.color,
+                brand: brand.name
+            }
+        } catch (err) {
+            if (!err.response) throw err
+            if (err.response.statusCode === 404) throw httpErrors.notFound()
+            throw err
         }
     })
 }
