cdd3a9de3e629cb0a34c4e5fdbe32f500717f716
DavideDR <d.dr@libero.it>
2023-02-28 19:10:33 +0100

    Completes fastify proxy single origin, multi route(2)

--- fastify-http-proxy-single-origin-multi-route/app.js
+++ fastify-http-proxy-single-origin-multi-route/app.js
@@ -1,11 +1,19 @@
 'use strict'
 
 const proxy = require('fastify-http-proxy')
+const sensible = require('fastify-sensible')
 
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
 
 module.exports = async function(fastify, opts) {
-    fastify.register(proxy, { upstream: 'https://news.ycombinator.com' })
+    fastify.register(sensible)
+    fastify.register(proxy, {
+        upstream: 'https://news.ycombinator.com',
+        async preHandler(request, reply) {
+            if (request.query.token !== 'abc')
+                throw fastify.httpErrors.unauthorized()
+        }
+    })
 
 }
